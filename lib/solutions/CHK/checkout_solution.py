1115b80fa5603229f385c472da248c1c94ffd097
sbx_user1051 <sbx_user1051@169.254.9.17>
2025-09-28 11:11:43 +0000

    Sun Sep 28 11:11:43 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,30 +13,34 @@ class CheckoutSolution:
     | D    | 15    |                |
     +------+-------+----------------+
     """
-    
-    # skus = unicode string
-    def checkout(self, skus: str) -> int:
-        
-        total_price = 0
-        
-        item_price_map = {
+    item_price_map = {
             "A" : 50,
             "B": 30,
             "C": 20,
             "D": 15
         }
         
-        item_specials_map = {
-            "A": [3, 130],
-            "B": [2, 45]
-        }
+    item_specials_map = {
+        "A": [3, 130],
+        "B": [2, 45]
+    }
+    
+    
+    # skus = unicode string
+    def checkout(self, skus: str) -> int:
         
+        total_price = 0
         sku_counter = Counter(skus)
             
         for sku, count in sku_counter.items():
-            if sku not in item_price_map:
+            if sku not in self.item_price_map:
                 return -1
-            if sku in item_specials_map:
+            total_price += self._calculate_sku_price(sku, count)
+        
+        return total_price
+                
+    def _calculate_sku_price(self, sku: str, count: int) -> int:
+        if sku in item_specials_map:
                 special_count, special_price = item_specials_map[sku]
                 remaining_count = count
                 while remaining_count >= special_count:
@@ -45,6 +49,7 @@ class CheckoutSolution:
                 total_price += remaining_count*item_price_map[sku]
             else:
                 total_price += count*item_price_map[sku]
+        
         
         return total_price
 
