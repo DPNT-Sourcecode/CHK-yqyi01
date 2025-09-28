2aacceab7b00b646fc557939f952f3fad537522d
sbx_user1051 <sbx_user1051@169.254.9.17>
2025-09-28 10:13:59 +0000

    Sun Sep 28 10:13:59 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,5 +2,9 @@
 class SumSolution:
     
     def compute(self, x: int, y: int) -> int:
-        return (x + y)
+        if (x >= 0) and (x <= 100) and (y <= 0) and (y >= 0):
+            return x + y
+        else:
+            return -1
+
 
--- test/solution_tests/SUM/test_sum_solution.py
+++ test/solution_tests/SUM/test_sum_solution.py
@@ -4,5 +4,8 @@ from solutions.SUM.sum_solution import SumSolution
 class TestSum():
     def test_sum(self):
         assert SumSolution().compute(1, 2) == 3
+        assert SumSolution().compute(0, 100) == 100
+        assert SumSolution().compute(1, 100) == 101
+
 
 
